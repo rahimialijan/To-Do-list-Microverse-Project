8661a1a740e065232d89e5db0aa737ef4464584b
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-05-11 00:09:08 +0500

    Add script code according ot my first reviewer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+/*
 import UI from './modules/UI.js';
 import Store from './modules/localstorage.js';
 import Todostatus from './modules/toDoStatus.js';
@@ -47,4 +48,44 @@ document.querySelector('#list-item').addEventListener('keypress', (e) => {
 document.getElementById('clear-btn').addEventListener('click', () => {
   Todostatus.cleartodoCompleted();
   window.location.reload();
+});
+*/
+// Define the to do tasks according to my code reveiwer
+const toDoTasks = [
+  {
+    description: 'task one',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'task two',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'task tree',
+    completed: false,
+    index: 2,
+  },
+];
+
+// difining the UL id
+const listItem = document.getElementById('list-item');
+
+// loop through the array of obj and creat li for each
+toDoTasks.forEach((todo) => {
+  // Create a new li element
+  const listElement = document.createElement('li');
+  const spanElement = document.createElement('span');
+  spanElement.textContent = todo.description;
+
+  const checkboxElement = document.createElement('input');
+  checkboxElement.type = 'checkbox';
+  listElement.appendChild(checkboxElement);
+  listElement.appendChild(spanElement);
+
+  if (todo.completed) {
+    listItem.classList.add('completed');
+  }
+  listItem.appendChild(listElement);
 });
